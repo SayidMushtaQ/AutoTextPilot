012da5a6d6351a9eff6fdc012a89d2327512b595
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-09-02 17:42:06 +0530

    Track and set Cursor movement

--- main.py
+++ main.py
@@ -1,2 +1,32 @@
+import pyautogui
+import time
+import pyperclip
 if __name__ == '__main__':
-    print("Hello")
+   
+    cursorLocation = pyautogui.position();
+    print(cursorLocation)
+
+    #  1251,1061
+    #  683,10
+    #  191,426
+    #  669,224
+    #  1220,892
+
+    pyautogui.click(1251,1061);
+    time.sleep(1);
+    pyautogui.click(683,10);
+    time.sleep(1);
+    pyautogui.click(191,426);
+    time.sleep(1);
+    print(cursorLocation)
+    pyautogui.moveTo(669,224)
+    pyautogui.dragTo(1220,892,duration=1.0,button='left')
+
+    pyautogui.hotkey('ctrl','c');
+    time.sleep(1);
+    text = pyperclip.paste();
+    print(text)
+
+
+
+    
